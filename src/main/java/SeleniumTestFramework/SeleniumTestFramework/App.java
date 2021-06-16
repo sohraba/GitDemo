4cc3e7284cce3bac91965164497c9f4213df3c1d
Sohrab <sohrab.ahmad@ihsmarkit.com>
2021-06-16 20:45:32 +0530

    First Commit

---
+++ src/main/java/SeleniumTestFramework/SeleniumTestFramework/App.java
@@ -0,0 +1,13 @@
+package SeleniumTestFramework.SeleniumTestFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/SeleniumTestFramework/SeleniumTestFramework/NewTest.java
@@ -0,0 +1,25 @@
+package SeleniumTestFramework.SeleniumTestFramework;
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;		
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;		
+public class NewTest {		
+	    private WebDriver driver;		
+		@Test				
+		public void testDemo() {	
+			driver.get("https://www.google.com/");  
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Google")); 		
+		}	
+		@BeforeTest
+		public void beforeTest() {	
+		    driver = new FirefoxDriver();  
+		}		
+		@AfterTest
+		public void afterTest() {
+			driver.quit();			
+		}		
+}	
