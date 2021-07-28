957521d7c3202df23df11f8765ee761edf84a12d
Sohrab <sohrab.ahmad@ihsmarkit.com>
2021-07-28 21:45:26 +0530

    first commit

--- src/test/java/org/seleniumhq/selenium/git_demo/AppTest.java
+++ src/test/java/org/seleniumhq/selenium/git_demo/AppTest.java
@@ -12,6 +12,7 @@ public class AppTest
   protected WebDriver driver;
   @Test
   public void testGoogle() throws InterruptedException{
+	  System.out.println("Testing");
 	  System.setProperty("webdriver.chrome.driver", "F:\\Backup\\MyCode\\chromedriver\\chromedriver.exe");
   }
   
