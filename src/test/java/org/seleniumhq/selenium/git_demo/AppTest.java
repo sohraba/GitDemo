bf8300316851e5f0aede5d854598aac34269d759
Sohrab <sohrab.ahmad@ihsmarkit.com>
2021-08-08 22:18:58 +0530

    updated test file

--- src/test/java/org/seleniumhq/selenium/git_demo/AppTest.java
+++ src/test/java/org/seleniumhq/selenium/git_demo/AppTest.java
@@ -12,7 +12,8 @@ public class AppTest
   protected WebDriver driver;
   @Test
   public void testGoogle() throws InterruptedException{
-	  System.out.println("Testing");
+	  System.out.println("new code");
+	  System.out.println("Testing123");
 	  System.setProperty("webdriver.chrome.driver", "F:\\Backup\\MyCode\\chromedriver\\chromedriver.exe");
   }
   
