d93030484ae365bb4391f5367220f88979066ee6
Sohrab <sohrab.ahmad@ihsmarkit.com>
2022-02-03 23:07:19 +0530

    Git commit 3rd Feb

--- src/test/java/org/seleniumhq/selenium/git_demo/AppTest.java
+++ src/test/java/org/seleniumhq/selenium/git_demo/AppTest.java
@@ -12,9 +12,6 @@ public class AppTest
   protected WebDriver driver;
   @Test
   public void testGoogle() throws InterruptedException{
-	  System.out.println("Testing");
-	  System.out.println("new code123");
-	  System.out.println("Testing");
 	  System.setProperty("webdriver.chrome.driver", "F:\\Backup\\MyCode\\chromedriver\\chromedriver.exe");
   }
   
