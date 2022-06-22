7f10729a33ec6e4f9baa2456bb4fbabdbbe210a6
Sohrab <sohrab.ahmad@ihsmarkit.com>
2022-06-22 21:52:11 +0530

    Commit 22nd June

--- src/main/java/org/seleniumhq/selenium/git_demo/App.java
+++ src/main/java/org/seleniumhq/selenium/git_demo/App.java
@@ -6,8 +6,12 @@ package org.seleniumhq.selenium.git_demo;
  */
 public class App 
 {
+	int x,int y,int sum;
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
+	public void sum(){
+		int sum=x+y;
+	}
 }
